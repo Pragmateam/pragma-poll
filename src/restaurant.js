da5ddef461478e3497f61daea2cbd00862023a3b
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2016-09-05 13:13:51 +1000

    chore: remove unnecessary files

--- src/restaurant.js
+++
@@ -1,8 +0,0 @@
-const EventBus = require('./event-bus');
-const UUID = require('./uuid');
-
-module.exports.create = function create () {
-  EventBus.emit('RestaurantCreated', {
-    uuid: UUID.generate()
-  });
-};
